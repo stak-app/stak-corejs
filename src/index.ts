e1a4e3ef1324fe606680e0253ebeb96d3b790fec
GitHub <noreply@github.com>
2024-05-19 16:15:01 +0000

    chore: update stak-core to v0.13.0
    Release-As: 0.13.0

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ export { auth } from "./packages/auth";
 export { config } from "./packages/config";
 export { core } from "./packages/core";
 export { modules } from "./packages/modules";
+export { organizations } from "./packages/organizations";
 export { projects } from "./packages/projects";
